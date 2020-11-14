62fb6ba25a560eb14730ec22006f268ba951577d
Treyway1 <treyvor.romero-jones@patelhs.org>
2020-11-14 02:59:34 +0000

    Day 2 Made Seat numbers and Scanner

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-     System.out.println(" Welcome to Ms.Benny's class, this is where everyone is sitting.");
+     System.out.println(" Welcome to Ms.Benny's class, this is where everyone is sitting.");// First System.out.print
+     int[] seatNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+     String[] names = new String [15];//String Here
+     Scanner scan = new Scanner(System.in);
+    
